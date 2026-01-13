824cbc3b8ace6bde09a500d35a856129e56ee218
Kevin Jesubalan <kevin.jesubalan@fulfil.com>
2026-01-13 05:41:05 -0800

    add upload cam_code thing

--- src/apps/fulfil/fulfil.mk
+++ src/apps/fulfil/fulfil.mk
@@ -25,8 +25,12 @@ app-$(APP_NAME):
 		$(RFSDIR)/etc/systemd/system/multi-user.target.wants/fulfil-firstboot.service
 
 	# 5. Install and enable the camera service
+	install -m 0755 $(CURDIR)/fulfil/files/upload_cam_code.sh \
+		$(RFSDIR)/opt/fulfil/upload_cam_code.sh
+
 	install -m 0644 $(CURDIR)/fulfil/files/fulfil-camera-install.service \
 		$(RFSDIR)/etc/systemd/system/fulfil-camera-install.service
+
 	ln -sf /etc/systemd/system/fulfil-camera-install.service \
 		$(RFSDIR)/etc/systemd/system/multi-user.target.wants/fulfil-camera-install.service
 
